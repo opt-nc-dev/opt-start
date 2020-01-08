ba8de5ec55c7db6653f2236d5a8757525b70a81a
sleborgne <sylvain.leborgne.ext@opt.nc>
2020-01-08 13:44:09 +1100

    chore(build): les classes utilisant des packages privées (artifactory) sont commentées

--- src/main/java/nc/opt/core/poc/app/service/indicator/ElasticHealthIndicator.java
+++ src/main/java/nc/opt/core/poc/app/service/indicator/ElasticHealthIndicator.java
@@ -1,9 +1,6 @@
 package nc.opt.core.poc.app.service.indicator;
 
-import nc.opt.core.poc.app.service.MailService;
-import nc.opt.core.search.elasticsearch.actuate.health.ElasticsearchHealthService;
-import nc.opt.core.search.elasticsearch.config.ConditionalOnOPTELS;
-import org.elasticsearch.client.RestHighLevelClient;
+//import org.elasticsearch.client.RestHighLevelClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
@@ -17,30 +14,31 @@ import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 
-@Component(value = "elasticSearch")
-// Si la configuration contient ES_CLUSTER_NODES alors on ne monte pas le composant
-// Car la variable d'env n'existe pas
-@ConditionalOnOPTELS
-public class ElasticHealthIndicator implements HealthIndicator {
-
-    private ElasticsearchHealthService elasticsearchHealthService;
-    private final Logger log = LoggerFactory.getLogger(ElasticHealthIndicator.class);
-
-    public ElasticHealthIndicator(ElasticsearchHealthService elasticsearchHealthService) {
-        this.elasticsearchHealthService = elasticsearchHealthService;
-    }
-
-    @Override
-    public Health health() {
-        String esClusterNode = System.getenv("ES_CLUSTER_NODES");
-        log.info("Health ES_CLUSTER_NODES " + esClusterNode);
-        if (StringUtils.isEmpty(esClusterNode))
-        {
-            log.info("Health ES_CLUSTER_NODES empty");
-            return Health.unknown().withDetail("ES_CLUSTER_NODES", "Valeur non présente").build();
-        } else {
-            log.info("Health ES_CLUSTER_NODES exist do Health");
-            return elasticsearchHealthService.health();
-        }
-    }
-}
+//@Component(value = "elasticSearch")
+//// Si la configuration contient ES_CLUSTER_NODES alors on ne monte pas le composant
+//// Car la variable d'env n'existe pas
+//@ConditionalOnOPTELS
+//public class ElasticHealthIndicator implements HealthIndicator {
+//
+//    private ElasticsearchHealthService elasticsearchHealthService;
+//
+//    private final Logger log = LoggerFactory.getLogger(ElasticHealthIndicator.class);
+//
+//    public ElasticHealthIndicator(ElasticsearchHealthService elasticsearchHealthService) {
+//        this.elasticsearchHealthService = elasticsearchHealthService;
+//    }
+//
+//    @Override
+//    public Health health() {
+//        String esClusterNode = System.getenv("ES_CLUSTER_NODES");
+//        log.info("Health ES_CLUSTER_NODES " + esClusterNode);
+//        if (StringUtils.isEmpty(esClusterNode))
+//        {
+//            log.info("Health ES_CLUSTER_NODES empty");
+//            return Health.unknown().withDetail("ES_CLUSTER_NODES", "Valeur non présente").build();
+//        } else {
+//            log.info("Health ES_CLUSTER_NODES exist do Health");
+//            return elasticsearchHealthService.health();
+//        }
+//    }
+//}
--- src/main/java/nc/opt/core/poc/app/service/indicator/KafkaHealthIndicator.java
+++ src/main/java/nc/opt/core/poc/app/service/indicator/KafkaHealthIndicator.java
@@ -1,7 +1,7 @@
 package nc.opt.core.poc.app.service.indicator;
 
-import nc.opt.core.kafka.actuate.KafkaBrokersHealthService;
-import nc.opt.core.kafka.config.ConditionalOnOPTKafka;
+//import nc.opt.core.kafka.actuate.KafkaBrokersHealthService;
+//import nc.opt.core.kafka.config.ConditionalOnOPTKafka;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.actuate.health.Health;
@@ -11,28 +11,28 @@ import org.springframework.util.StringUtils;
 
 // Si la configuration contient KAFKA_BROKERS_HOST alors on ne monte pas le composant
 // Car la variable d'env n'existe pas
-@ConditionalOnOPTKafka
-@Component(value = "kafka")
-public class KafkaHealthIndicator implements HealthIndicator {
-
-    private final Logger log = LoggerFactory.getLogger(KafkaHealthIndicator.class);
-    private final KafkaBrokersHealthService kafkaBrokersHealthService;
-
-    public KafkaHealthIndicator(KafkaBrokersHealthService kafkaBrokersHealthService) {
-        this.kafkaBrokersHealthService = kafkaBrokersHealthService;
-    }
-
-    @Override
-    public Health health() {
-        String kafkaBrokerHost = System.getenv("KAFKA_BROKERS_HOST");
-        log.info("Health KAFKA_BROKERS_HOST " + kafkaBrokerHost);
-        if (StringUtils.isEmpty(kafkaBrokerHost))
-        {
-            log.info("Health KAFKA_BROKERS_HOST empty");
-            return Health.unknown().withDetail("KAFKA_BROKERS_HOST", "Valeur non présente").build();
-        } else {
-            log.info("Health KAFKA_BROKERS_HOST exist do Health");
-            return kafkaBrokersHealthService.health();
-        }
-    }
-}
+//@ConditionalOnOPTKafka
+//@Component(value = "kafka")
+//public class KafkaHealthIndicator implements HealthIndicator {En
+//
+//    private final Logger log = LoggerFactory.getLogger(KafkaHealthIndicator.class);
+//    private final KafkaBrokersHealthService kafkaBrokersHealthService;
+//
+//    public KafkaHealthIndicator(KafkaBrokersHealthService kafkaBrokersHealthService) {
+//        this.kafkaBrokersHealthService = kafkaBrokersHealthService;
+//    }
+//
+//    @Override
+//    public Health health() {
+//        String kafkaBrokerHost = System.getenv("KAFKA_BROKERS_HOST");
+//        log.info("Health KAFKA_BROKERS_HOST " + kafkaBrokerHost);
+//        if (StringUtils.isEmpty(kafkaBrokerHost))
+//        {
+//            log.info("Health KAFKA_BROKERS_HOST empty");
+//            return Health.unknown().withDetail("KAFKA_BROKERS_HOST", "Valeur non présente").build();
+//        } else {
+//            log.info("Health KAFKA_BROKERS_HOST exist do Health");
+//            return kafkaBrokersHealthService.health();
+//        }
+//    }
+//}
